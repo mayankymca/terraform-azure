72e80203380dd0046691dc838125d3c98527d372
Mayank Ahuja <mayank.ymca@gmail.com>
2023-07-16 11:52:37 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "rgname" {
+  description = "Resource Group Name"
+  default     = "testing-tf"
+  type        = string
+}
+variable "location" {
+  description = "Azure location"
+  default     = "East US"
+  type        = string
+}
